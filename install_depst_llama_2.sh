452bda1d83b12df73adb3a2cd95187c411f0210f
Szymon Ruciński <44577190+szymonrucinski@users.noreply.github.com>
2023-07-20 11:24:35 +0200

    udpate llama_2 install

--- install_depst_llama_2.sh
+++ install_depst_llama_2.sh
@@ -1,8 +1,8 @@
-!pip install -q -U bitsandbytes==0.40.2
-!pip install -q -U transformers==4.31.0
-!pip install -q -U peft==0.4.0
-!pip install -q -U accelerate==0.21.0
-!pip install -q -U datasets
-!pip install -q -U trl==0.4.7
-!pip install wandb
-!pip install sentencepiece
+pip install -q -U bitsandbytes==0.40.2
+pip install -q -U transformers==4.31.0
+pip install -q -U peft==0.4.0
+pip install -q -U accelerate==0.21.0
+pip install -q -U datasets
+pip install -q -U trl==0.4.7
+pip install wandb
+pip install sentencepiece
