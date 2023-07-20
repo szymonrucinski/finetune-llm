9617593de13bc552dc3080ddb0cc5217cccf45f2
Szymon Ruciński <44577190+szymonrucinski@users.noreply.github.com>
2023-07-20 11:08:15 +0200

    Add scripts

---
+++ install_deps.sh
@@ -0,0 +1,8 @@
+pip install -q -U bitsandbytes
+pip install -q -U git+https://github.com/huggingface/transformers.git
+pip install -q -U git+https://github.com/huggingface/peft.git
+pip install -q -U git+https://github.com/huggingface/accelerate.git
+pip install -q -U datasets
+pip install -q -U git+https://github.com/lvwerra/trl.git
+pip install wandb
+pip install sentencepiece
---
+++ start_training.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/lvwerra/trl/main/examples/scripts/sft_trainer.py -o sft_trainer.py
+curl https://gist.githubusercontent.com/younesbelkada/9f7f75c94bdc1981c8ca5cc937d4a4da/raw/6f027020ed295447a2c476c610b99f2441b45e39/finetune_llama_v2.py -o finetune_llama_v2.py
+
+python finetune_llama_2
