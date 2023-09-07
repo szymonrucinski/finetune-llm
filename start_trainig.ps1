946ca696b100fc08c25076e39421e80cdce17cf0
Szymon Rucinski <szymonrucinski@icloud.com>
2023-09-07 17:34:41 +0200

    Add merging and quantize_llama

---
+++ start_trainig.ps1
@@ -0,0 +1,38 @@
+# Younes's script
+$adaptername = "szymonrucinski/krakowiak-7b"
+$model_name = "meta-llama/Llama-2-7b-hf"
+$dataset_name = "szymonindy/ociepa-raw-self-generated-instructions-pl"
+$quant_type = "q4_k_m"
+
+# Download finetune_llama_v2.py
+Invoke-WebRequest -Uri "https://gist.githubusercontent.com/younesbelkada/9f7f75c94bdc1981c8ca5cc937d4a4da/raw/6f027020ed295447a2c476c610b99f2441b45e39/finetune_llama_v2.py" -OutFile "finetune_llama_v2.py"
+
+# Run finetune_llama_v2.py
+python finetune_llama_v2.py --model_name $model_name --dataset_name $dataset_name --per_device_train_batch_size 8 --use_4bit --bf16 --bnb_4bit_compute_dtype bfloat16 --merge_and_push
+
+# Run merge_peft_adapters.py
+python merge_peft_adapters.py --base_model_name_or_path meta-llama/Llama-2-7b-hf --peft_model_path $adaptername --output_dir ./models/merged/ --device auto
+
+# Install and compile llama.cpp
+git clone https://github.com/ggerganov/llama.cpp
+cd llama.cpp
+git pull
+make clean
+$env:LLAMA_CUBLAS=1
+make
+pip install -r llama.cpp/requirements.txt
+
+# Convert model to gguf 16bit
+python llama.cpp/convert.py ./models/merged --outtype f16 --outfile ./models/gguf/merged-model.gguf.fp16.bin
+
+# Quantize model
+./build/Release/quantize.exe ./merged-model/ $quant_type ./gguf/merged-model.q4_k_m.gguf.bin
+
+# Convert model to f16
+python .\llama.cpp\convert.py ./merged-model/ --outtype f16 --outfile ./gguf/merged-model.fp16.bin
+
+# Run main.exe
+.\llama.cpp\build\bin\Release\main.exe .\gguf\merged-model.q4_k_m.fp16.bin q4_k_m -n 128
+
+# Run quantize.exe
+.\llama.cpp\build\bin\Release\quantize.exe .\models\gguf\krakowiak-7b.gguf.fp16.bin .\models\gguf\krakowiak-7b.gguf.q4_k_m.bin $quant_type
