0f5cb0d302fdb59aa433bc3fbc9d160db934fde7
Szymon Ruciński <44577190+szymonrucinski@users.noreply.github.com>
2023-10-14 13:51:35 +0200

    Re-order-data

---
+++ src/train-qlora/merge_peft_adapters.py
@@ -0,0 +1,54 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+import torch
+
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_model_name_or_path", type=str)
+    parser.add_argument("--peft_model_path", type=str)
+    parser.add_argument("--output_dir", type=str)
+    parser.add_argument("--device", type=str, default="auto")
+    parser.add_argument("--push_to_hub", action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    if args.device == "auto":
+        device_arg = {"device_map": "auto"}
+    else:
+        device_arg = {"device_map": {"": args.device}}
+
+    print(f"Loading base model: {args.base_model_name_or_path}")
+    base_model = AutoModelForCausalLM.from_pretrained(
+        args.base_model_name_or_path,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16,
+        **device_arg,
+    )
+
+    print(f"Loading PEFT: {args.peft_model_path}")
+    model = PeftModel.from_pretrained(base_model, args.peft_model_path, **device_arg)
+    print("Running merge_and_unload")
+    model = model.merge_and_unload()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model_name_or_path)
+
+    if args.push_to_hub:
+        print("Saving to hub ...")
+        model.push_to_hub(f"{args.output_dir}", use_temp_dir=False)
+        tokenizer.push_to_hub(f"{args.output_dir}", use_temp_dir=False)
+    else:
+        model.save_pretrained(f"{args.output_dir}")
+        tokenizer.save_pretrained(f"{args.output_dir}")
+        print(f"Model saved to {args.output_dir}")
+
+
+if __name__ == "__main__":
+    main()
--- train-qlora/install_deps.sh
+++
@@ -1,9 +0,0 @@
-pip install -q -U bitsandbytes
-pip install -q -U git+https://github.com/huggingface/transformers.git
-pip install -q -U git+https://github.com/huggingface/peft.git
-pip install -q -U git+https://github.com/huggingface/accelerate.git
-pip install -q -U datasets
-pip install -q -U git+https://github.com/lvwerra/trl.git
-pip install scipy
-pip install wandb
-pip install sentencepiece
